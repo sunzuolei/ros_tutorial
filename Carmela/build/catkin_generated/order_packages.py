f091bd359538b1bbc8527166882e46f80a57177a
yp233 <30382622+yp233@users.noreply.github.com>
2017-07-23 20:04:43 +0800

    carmela

---
+++ Carmela/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/yp2/Desktop/carmela/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/yp2/Desktop/carmela/devel;/home/yp2/Desktop/chapter2/devel;/home/yp2/Desktop/model/devel;/home/yp2/Desktop/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/yp2/Desktop/carmela/devel;/home/yp2/Desktop/chapter2/devel;/home/yp2/Desktop/model/devel;/home/yp2/Desktop/catkin_ws/devel;/opt/ros/kinetic" != "" else []
